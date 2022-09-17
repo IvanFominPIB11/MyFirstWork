cdfadd08fe55e3f82783cac31dc5e195b6a126a4
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2022-09-17 09:29:34 +0700

    First

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
